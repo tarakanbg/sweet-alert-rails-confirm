cfc381d8e6031bf3ff9d0c523dc3d5382eb81f72
Svilen Vassilev <tarakan.sv@gmail.com>
2015-02-24 12:36:12 +0200

    Update view_helpers.rb

--- lib/sweet-alert-confirm/view_helpers.rb
+++ lib/sweet-alert-confirm/view_helpers.rb
@@ -9,6 +9,16 @@ module SweetAlertConfirm
       end
       super *args, &block
     end
+    
+    def button_to(*args, &block)
+      html_options = args[block_given? ? 1 : 2] || {}
+
+      if options_has_confirm?(html_options)
+        html_options['data-sweet-alert-confirm'] = html_options.delete(:confirm) ||
+                    html_options[:data].delete(:confirm)
+      end
+      super *args, &block
+    end
 
     def submit_tag(value = "Save changes", options = {})
       options['data-sweet-alert-confirm'] = options.delete(:confirm) || options[:data].delete(:confirm) if options_has_confirm?(options)
